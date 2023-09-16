ae0dee2d933002618bb2a8717ca0c2ce7dabecec
BowmanKensie <143542822+BowmanKensie@users.noreply.github.com>
2023-09-15 21:29:41 -0600

    Maybe THIS time

--- Assignments/Assignment_4/Assignment_4BANKS.R
+++ Assignments/Assignment_4/Assignment_4BANKS.R
@@ -1,13 +1,13 @@
 library(tidyverse)
-dat <- read_csv("./Assignments/Assignment_4/AlgaeFake.csv")
-dat$`Cells Present`
-ggplot(dat,aes(x=Temperature,y=`Cells Present`)) +
+getwd()
+Algae <- read_csv("C:/Users/menzi/BIOL3100/Data_Course_BANKS/Assignments/Assignment_4/FakeAlgae - Sheet1.csv")
+ggplot(Algae,aes(x=Temperature,y=Cells)) +
   geom_point(color="Turquoise", size= 4, alpha=.25) +
   geom_smooth(method="lm",color="lime green",se=FALSE, linetype=1) +
   labs(title= "Fake Utah Lake Algae",
-       subtitle = "From the Fake Algae Dataset",
+       subtitle = "From Fake Algae Dataset",
        x= "Temperature",
-       y= 'Cells Present',
+       y= "Cells",
        caption= "UTAH") +
-  theme_grey()
+  theme_classic()
 
